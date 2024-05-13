d92b2e49de198708c62bb962283d9c862412d719
hayoungbin <zinks1998@gmail.com>
2024-05-13 13:13:40 +0900

    필수 요구사항 3 , 4
    완료

---
+++ Assets/Scripts/UI/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform camT;
+
+    [SerializeField] private Vector3 CamOffset;
+
+    void Start()
+    {
+        if (Camera.main != null) camT = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        var position = transform.position;
+        position.z = -10;
+        camT.position = position + CamOffset;
+    }
+}
